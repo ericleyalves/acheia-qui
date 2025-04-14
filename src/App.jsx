1d5079a023733e66ad30b0f9698eca05c9c3ccb0
ericleyalves <Erickalvesaju@gmail.com>
2025-04-14 01:28:53 -0300

    subindo projeto achei-aqui completo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-2xl font-bold text-purple-600">Achei Aqui</h1>
+      <p>Compra e venda por bairro</p>
+    </div>
+  );
+}
